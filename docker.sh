fc4b93b3cd5a0f951f564fe8f26cb7d73cfa9c18
Simon Strubel <simon@strubel.io>
2025-06-18 15:28:54 +0200

    Added client and docker integration

--- docker.sh
+++ docker.sh
@@ -3,3 +3,9 @@ cp target/DTUstatistic.jar ../docker/api/DTUstatistic.jar
 cd ../docker/api
 docker build --no-cache -t dtu-statistic-api:latest .
 docker image save dtu-statistic-api -o ../dtu-statistic-api.img
+
+
+cd ../../client && npm run prod
+cd ../docker/client
+docker build --no-cache -t dtu-statistic-client:latest .
+docker image save dtu-statistic-api -o ../dtu-statistic-client.img
